aad5a1b9556e70574d601d2382f8e6712be81bf7
mark.davison <mark.davison@taxlab.online>
2022-07-21 20:15:52 +1200

    Add coverage for CookieHandler

--- src/mark.davison.common/mark.davison.common.client.tests/Usings.cs
+++ src/mark.davison.common/mark.davison.common.client.tests/Usings.cs
@@ -6,3 +6,4 @@ global using mark.davison.common.CQRS;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using Moq;
+global using System.Net;
