7aa6fd034ec8b43e9e0d23820f54452204093f2d
mark.davison <mark.davison@taxlab.online>
2022-07-26 15:00:56 +1200

    Expose health dependency inversion extensions

--- src/mark.davison.common/mark.davison.common.server/DependencyInversionExtensions.cs
+++ src/mark.davison.common/mark.davison.common.server/DependencyInversionExtensions.cs
@@ -101,4 +101,44 @@ public static class DependencyInversionExtensions
         return services;
     }
 
+    public static void ConfigureHealthCheckServices(this IServiceCollection services)
+    {
+        services.ConfigureHealthCheckServices<GenericApplicationHealthStateHostedService>();
+    }
+
+    public static void ConfigureHealthCheckServices<THealthHosted>(this IServiceCollection services)
+        where THealthHosted : class, IApplicationHealthStateHostedService
+    {
+        services.AddSingleton<IApplicationHealthState, ApplicationHealthState>();
+
+        services.AddHealthChecks()
+            .AddCheck<StartupHealthCheck>(StartupHealthCheck.Name)
+            .AddCheck<LiveHealthCheck>(LiveHealthCheck.Name)
+            .AddCheck<ReadyHealthCheck>(ReadyHealthCheck.Name);
+
+        services.AddHostedService<THealthHosted>();
+    }
+
+    public static void MapHealthChecks(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints
+            .MapHealthChecks("/health/startup", new HealthCheckOptions
+            {
+                Predicate = r => r.Name == StartupHealthCheck.Name
+            })
+            .AllowAnonymous();
+        endpoints
+            .MapHealthChecks("/health/liveness", new HealthCheckOptions
+            {
+                Predicate = r => r.Name == LiveHealthCheck.Name
+            })
+            .AllowAnonymous();
+        endpoints
+            .MapHealthChecks("/health/readiness", new HealthCheckOptions
+            {
+                Predicate = r => r.Name == ReadyHealthCheck.Name
+            })
+            .AllowAnonymous();
+    }
+
 }
