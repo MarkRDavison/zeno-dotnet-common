5b472e6f86e6c87633dfc404209a13cb1ddabdfe
mark.davison <mark.davison@taxlab.online>
2022-11-19 21:55:53 +1300

    Add mockjwt tokens

--- src/mark.davison.common/mark.davison.common.test/Usings.cs
+++ src/mark.davison.common/mark.davison.common.test/Usings.cs
@@ -1,2 +1,6 @@
-﻿global using System.Diagnostics.CodeAnalysis;
+﻿global using Microsoft.IdentityModel.Tokens;
+global using System.Diagnostics.CodeAnalysis;
+global using System.IdentityModel.Tokens.Jwt;
 global using System.Net;
+global using System.Security.Claims;
+global using System.Security.Cryptography;
