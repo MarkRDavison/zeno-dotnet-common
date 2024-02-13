e202607ed0fd4988685113f553845e0ecab35ace
mark.davison <markdavison0+github@gmail.com>
2024-02-13 17:47:25 +1300

    Upgrade nuget and add TestHttpMessageHandler

---
+++ src/mark.davison.common/Tests/mark.davison.common.tests/Utility/TestHttpMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace mark.davison.common.tests.Utility;
+
+public class TestHttpMessageHandler : HttpMessageHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        if (Callback != null)
+        {
+            return await Callback(request);
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public Func<HttpRequestMessage, Task<HttpResponseMessage>>? Callback { get; set; }
+}
