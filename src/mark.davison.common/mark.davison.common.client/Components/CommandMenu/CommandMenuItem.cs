bebfe141f0a5e81fc91e4688551c453044e8c3db
mark.davison <markdavison0+github@gmail.com>
2024-06-12 10:26:12 +1200

    add divider/disabled to command menu

--- src/mark.davison.common/mark.davison.common.client/Components/CommandMenu/CommandMenuItem.cs
+++ src/mark.davison.common/mark.davison.common.client/Components/CommandMenu/CommandMenuItem.cs
@@ -4,5 +4,7 @@ public class CommandMenuItem
 {
     public string Text { get; set; } = string.Empty;
     public string? Id { get; set; }
+    public bool Disabled { get; set; }
+    public bool Divider { get; set; }
     public List<CommandMenuItem>? Children { get; set; }
 }
