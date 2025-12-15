a3a79458082559c3e4607661a498fdd18eda393a
mark.davison <markdavison0+github@gmail.com>
2025-12-15 18:58:15 +1300

    Add ClickableIcon

---
+++ src/mark.davison.common.client.web/Components/ClickableIcon/ClickableIcon.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace mark.davison.common.client.web.Components;
+
+public partial class ClickableIcon
+{
+    [Parameter, EditorRequired]
+    public required string ActiveIcon { get; set; }
+
+    [Parameter, EditorRequired]
+    public required string InactiveIcon { get; set; }
+
+    [Parameter, EditorRequired]
+    public required string ActiveColour { get; set; }
+
+    [Parameter, EditorRequired]
+    public required string InactiveColour { get; set; }
+
+    [Parameter]
+    public string? ActiveTooltip { get; set; }
+
+    [Parameter]
+    public string? InactiveTooltip { get; set; }
+
+    [Parameter, EditorRequired]
+    public required EventCallback<bool> OnClick { get; set; }
+
+    [Parameter]
+    public bool Value { get; set; }
+
+    private string _activeStyle => $"fill: {ActiveColour}";
+    private string _inactiveStyle => $"fill: {(_hovered ? ActiveColour : InactiveColour)}";
+
+    private bool _hovered;
+
+    private void OnHoverEnter()
+    {
+        _hovered = true;
+    }
+
+    private void OnHoverExit()
+    {
+        _hovered = false;
+    }
+
+    private async Task OnClickIcon(bool setActive)
+    {
+        await OnClick.InvokeAsync(setActive);
+        Value = setActive;
+        await InvokeAsync(StateHasChanged);
+    }
+}
