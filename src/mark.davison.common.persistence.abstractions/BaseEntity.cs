ed8ec2fe0f01d91889490c7728acf7021aab474e
mark.davison <markdavison0+github@gmail.com>
2025-12-31 12:56:58 +1300

    use DateTimeOffset for created/lastmodified

--- src/mark.davison.common.persistence.abstractions/BaseEntity.cs
+++ src/mark.davison.common.persistence.abstractions/BaseEntity.cs
@@ -3,7 +3,7 @@
 public class BaseEntity
 {
     public required Guid Id { get; set; }
-    public required DateTime Created { get; set; }
-    public required DateTime LastModified { get; set; }
+    public required DateTimeOffset Created { get; set; }
+    public required DateTimeOffset LastModified { get; set; }
     public required Guid UserId { get; set; }
 }
