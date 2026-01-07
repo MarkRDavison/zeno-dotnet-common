dafdb6807f8b6bb77bb5a9199f59028980a30900
mark.davison <markdavison0+github@gmail.com>
2026-01-08 12:53:19 +1300

    Fix user dto

--- src/mark.davison.common.authentication.server/Models/UserDto.cs
+++ src/mark.davison.common.authentication.server/Models/UserDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace mark.davison.common.authentication.server.Models;
 
-public record UserDto(Guid Id, Guid TenantId, string Email, string DisplayName, bool IsActive, DateTime CreatedAt, DateTime LastModified);
+public record UserDto(Guid Id, Guid TenantId, string Email, string DisplayName, bool IsActive, DateTimeOffset CreatedAt, DateTimeOffset LastModified);
